e1607d7b73a80bc5f7aae7871ff8b1ff8fb01b94
Rabbi Khan <Krabby2244@gmail.com>
2026-05-24 21:29:45 +0600

    logger file remove

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import express, {
   type Request,
   type Response,
 } from "express";
-import logger from "./middleware/logger";
 import authRoutes from "./api/routes/auth.routes";
 import cookieParser from "cookie-parser";
 import issueRoutes from "./api/routes/issue.routes"
@@ -13,7 +12,6 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
-app.use(logger);
 app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
