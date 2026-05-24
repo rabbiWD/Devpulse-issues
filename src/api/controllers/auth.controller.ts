94ce9d5e4c46a6688a8f29058fe298fbc82b57df
Rabbi Khan <Krabby2244@gmail.com>
2026-05-24 17:53:33 +0600

    update auth controller logic and improve request handling

--- src/api/controllers/auth.controller.ts
+++ src/api/controllers/auth.controller.ts
@@ -6,12 +6,14 @@ export const signup = async (req: Request, res: Response) => {
   try {
     const user = await authService.createUser(req.body);
     res.status(201).json({
+      success: true,
       message: "User created successfully",
       data: user.rows[0],
     });
   } catch (error: any) {
     console.error("Error creating user:", error);
     res.status(500).json({
+      success: false,
       message: error.message,
       error: error,
     });
@@ -28,13 +30,9 @@ export const login = async (req: Request, res: Response) => {
     res.status(200).json({
       success: true,
       message: "User login successfully",
-      data: {
-        user,
-        tokens,
-      },
+      data: {user, tokens,},
     });
   } catch (error: any) {
-    console.error("Error logging in:", error);
     res.status(500).json({
       success: false,
       message: error.message,
